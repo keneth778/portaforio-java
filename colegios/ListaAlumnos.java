48c0205cfc9286e6eff8a330c02a8ad9ef1f6597
keneth <keneth.javier90@gmail.com>
2024-06-19 09:30:30 -0600

    escuela

--- colegios/ListaAlumnos.java
+++ colegios/ListaAlumnos.java
@@ -7,7 +7,7 @@ public class ListaAlumnos {
     public static void program() {
         System.out.println("Información de alumnos y cursos:");
 
-        // Crear 5 estudiantes
+        //estudiantes
         List<Estudiante> estudiantes = new ArrayList<>();
         estudiantes.add(new Estudiante("Pablo", "P4blo@scl.gt", 7, true));
         estudiantes.add(new Estudiante("Ana Lopez", "ana.lopez@example.com", 8, true));
@@ -15,23 +15,23 @@ public class ListaAlumnos {
         estudiantes.add(new Estudiante("Laura Garcia", "laura.garcia@example.com", 8, true));
         estudiantes.add(new Estudiante("Luis Gomez", "luis.gomez@example.com", 7, false));
 
-        // Crear 4 cursos
+        //cursos
         List<PrimeroPrimaria> cursos = new ArrayList<>();
         cursos.add(new PrimeroPrimaria("Matemáticas", "Primero Primaria", "3"));
         cursos.add(new PrimeroPrimaria("Lengua", "Primero Primaria", "3"));
         cursos.add(new PrimeroPrimaria("Ciencias", "Segundo Primaria", "3"));
         cursos.add(new PrimeroPrimaria("Inglés", "Segundo Primaria", "3"));
 
-        // Imprimir listado de alumnos
+        //alumnos
         System.out.println("\nListado de alumnos:");
         for (Estudiante estudiante : estudiantes) {
             System.out.println("Nombre: " + estudiante.getNombre() + ", Correo: " + estudiante.getCorreo());
         }
 
-        // Imprimir listado de cursos
+        // lista de cursos
         System.out.println("\nListado de cursos:");
         for (PrimeroPrimaria curso : cursos) {
-            System.out.println("Nombre: " + curso.getNombre() + ", Categoría: " + curso.getCategoria() + ", Periodo: " + curso.getPeriodo());
+            System.out.println("Nombre: " + curso.getNombre());
         }
     }
 
